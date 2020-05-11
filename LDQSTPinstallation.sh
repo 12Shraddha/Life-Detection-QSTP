3102ac4160c7fff793cd99fd622a4bc352eb8850
Ithihas Madala <58927988+helloithihas@users.noreply.github.com>
2020-05-11 18:29:42 +0530

    Add files via upload

---
+++ LDQSTPinstallation.sh
@@ -0,0 +1,13 @@
+pip install opencv-python-headless
+python -m pip install --user numpy scipy matplotlib ipython jupyter pandas sympy nose
+pip install notebook
+pip install scikit-learn
+pip install ipywidgets
+jupyter nbextension enable --py widgetsnbextension
+pip install nbinteract
+
+#Linux or Mac uncomment the line below
+#pip install torch torchvision
+
+#Windows uncomment the line below
+#pip install torch===1.5.0 torchvision===0.6.0 -f https://download.pytorch.org/whl/torch_stable.html
---
+++ LDQSTPinstallation_py2and3.sh
@@ -0,0 +1,27 @@
+pip install opencv-python-headless
+python -m pip install --user numpy scipy matplotlib ipython jupyter pandas sympy nose
+pip install notebook
+pip install scikit-learn
+pip install ipywidgets
+jupyter nbextension enable --py widgetsnbextension
+pip install nbinteract
+
+#Linux or Mac
+#pip install torch torchvision
+
+#Windows uncomment the line below
+#pip install torch===1.5.0 torchvision===0.6.0 -f https://download.pytorch.org/whl/torch_stable.html
+
+pip3 install opencv-python-headless
+python3 -m pip3 install --user numpy scipy matplotlib ipython jupyter pandas sympy nose
+pip3 install notebook
+pip3 install scikit-learn
+pip3 install ipywidgets
+jupyter nbextension enable --py widgetsnbextension
+pip3 install nbinteract
+
+#Linux or Mac uncomment the line below
+#pip install torch torchvision
+
+#Windows uncomment the line below
+#pip3 install torch===1.5.0 torchvision===0.6.0 -f https://download.pytorch.org/whl/torch_stable.html
